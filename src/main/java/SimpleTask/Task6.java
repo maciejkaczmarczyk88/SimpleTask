27d6e4c31c7553d38d18bb996f6439e9970a5c87
Maciek Kacz <maciek.kacz@wp.pl>
2023-01-23 17:14:06 +0100

    use if

---
+++ src/main/java/SimpleTask/Task6.java
@@ -0,0 +1,11 @@
+package SimpleTask;
+
+public class Task6 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return (talking && (hour < 7 || hour > 20));
+    }
+}
