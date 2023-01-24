66c091d09f9195117cdca821dcfac68d8b717bf4
Maciek Kacz <maciek.kacz@wp.pl>
2023-01-24 20:59:46 +0100

    use if

---
+++ src/main/java/SimpleTask/Task12.java
@@ -0,0 +1,24 @@
+package SimpleTask;
+//Given a string, return a new string where the first and last chars have been exchanged.
+
+public class Task12 {
+    public static void main(String[] args) {
+        String hello_world = frontBack("Hello World");
+        String hello_world2 = frontBack("a");
+        System.out.println(hello_world);
+        System.out.println(hello_world2);
+    }
+
+    public String frontBack(String str) {
+        String substring;
+        char first;
+        char last;
+        if (str.length() == 1 || str.length() == 0)
+            return str;
+        else {
+            first = str.charAt(0);
+            last = str.charAt(str.length() - 1);
+            substring = str.substring(1, str.length() - 1);
+        }
+        return last + substring + first;
+    }
