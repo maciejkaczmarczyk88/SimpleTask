3d48a44d187ad9f116adc565e2794644e66e7e58
Maciek Kacz <maciek.kacz@wp.pl>
2023-01-30 22:55:19 +0100

    Array and loop

---
+++ src/main/java/MediumTask/Task3.java
@@ -0,0 +1,39 @@
+package MediumTask;
+
+public class Task3 {
+
+    //Given a number n, create and return a new string array of length n, containing the strings "0", "1" "2" ..
+    // through n-1. N may be 0, in which case just return a length 0 array. Note: String.valueOf(xxx)
+    // will make the String form of most types. The syntax to make a new string array is: new String[desired_length]
+    // (See also: FizzBuzz Code)
+
+    public String[] fizzArray2(int[] n) {
+        String[] list = new String[n.length];
+        for (int i = 0; i < n.length; i++) {
+            String newStringList = String.valueOf(i);
+            list[i] = newStringList;
+        }
+        return list;
+    }
+
+    //Given an array of ints, return true if the array contains two 7's next to each other, or there are two 7's
+    // separated by one element, such as with {7, 1, 7}.
+    public boolean has77(int[] nums) {
+        boolean result = false;
+        for (int i = 0; i < nums.length; i++) {
+            try {
+                if (nums[i] == 7 && nums[i + 1] == 7) {
+                    result = true;
+                    break;
+                }
+                if (nums[i] == 7 && nums[i + 2] == 7) {
+                    result = true;
+                    break;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return false;
+            }
+        }
+        return result;
+    }
+}
--- src/main/java/MediumTask/TestClass.java
+++ src/main/java/MediumTask/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 
     public static Task1 task1 = new Task1();
     public static Task2 task2 = new Task2();
+    public static Task3 task3 = new Task3();
 
 
     public static void main(String[] args) {
@@ -16,11 +17,17 @@ public class TestClass {
       //  boolean dogcat2 = task2.catDog("dogcaa");
       //  System.out.println(dogcat1);
       //  System.out.println(dogcat2);
-        int table[] =  {2,0,6,5,1,7,3,9,0};
-        int size = task2.countEvens(table);
-        System.out.println(size);
+        int table[] =  {2, 7, 2, 2, 7, 7};
+        int table1[] =  {1, 7, 1, 1, 7};
+        //  int size = task2.countEvens(table);
+        // System.out.println(size);
 
-        int difference = task2.bigDiff(table);
-        System.out.println(difference);
+        // int difference = task2.bigDiff(table);
+        // System.out.println(difference);
+
+        boolean tab1 = task3.has77(table);
+        boolean tab2 = task3.has77(table1);
+        System.out.println(tab1);
+        System.out.println(tab2);
     }
 }
